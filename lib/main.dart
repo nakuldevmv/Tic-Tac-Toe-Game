408a39c413864f8a4e79b7c83120b6eaacbc78a4
Nakul Dev <nakuldev1561@gmail.com>
2024-10-26 19:13:27 +0530

    deploy

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/webView/webViewContainer.dart';
 
 import 'TicTacToe/tictactoe.dart';
 
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: TicTacToe(),
+      home: WebView(child: TicTacToe()),
     );
   }
 }
